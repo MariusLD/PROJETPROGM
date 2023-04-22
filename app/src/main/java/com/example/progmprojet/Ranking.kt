0a67c5bc7fbee688eedd6eeef50da344d74887f6
Marius Le Douarin <marius.lledouarin@hotmail.fr>
2023-04-22 19:31:07 +0200

    Add Ranking Activity(+ UI) with dynamic leaderboard display using FirebaseDB

--- app/src/main/java/com/example/progmprojet/Ranking.kt
+++ app/src/main/java/com/example/progmprojet/Ranking.kt
@@ -1,9 +1,13 @@
 package com.example.progmprojet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
@@ -11,10 +15,18 @@ class Ranking : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ranking)
-        val dbRef = FirebaseDatabase.getInstance().getReference()
+        val type = intent.getStringExtra("type")
+        val dbRef = FirebaseDatabase.getInstance().getReference().child(type!!)
 
-        val rankingContainer: LinearLayout = findViewById(R.id.object_list_container)
-
+        val rankingContainer: LinearLayout = findViewById(R.id.ranking_container)
+        val view = layoutInflater.inflate(R.layout.rank_list_item, null)
+        val nameView: TextView = view.findViewById(R.id.tv_name)
+        val scoreView: TextView = view.findViewById(R.id.tv_score)
+        val rankView : TextView = view.findViewById(R.id.tv_rank)
+        rankView.text = "Rank"
+        nameView.text = "Player"
+        scoreView.text = "Score"
+        rankingContainer.addView(view)
         dbRef.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 val objectsList = mutableListOf<Pair<String, Int>>()
@@ -28,12 +40,24 @@ class Ranking : AppCompatActivity() {
                 // Sort the objectsList in descending order of their scores
                 objectsList.sortByDescending { it.second }
 
+                var rank = 1
                 for (obj in objectsList) {
                     // Create a view for the object and its score
-                    val view = layoutInflater.inflate(R.layout.object_list_item, null)
-                    val nameView: TextView = view.findViewById(R.id.object_name)
-                    val scoreView: TextView = view.findViewById(R.id.object_score)
-                    nameView.text = obj.first
+                    val view = layoutInflater.inflate(R.layout.rank_list_item, null)
+                    val nameView: TextView = view.findViewById(R.id.tv_name)
+                    val scoreView: TextView = view.findViewById(R.id.tv_score)
+                    val rankView : TextView = view.findViewById(R.id.tv_rank)
+                    if (rank==1) {
+                        rankView.text = "🥇"
+                    } else if (rank==2) {
+                        rankView.text = "🥈"
+                    } else if (rank==3) {
+                        rankView.text = "🥉"
+                    } else {
+                        rankView.text = rank.toString()
+                    }
+                    rank++
+                    nameView.text = obj.first // Player ID 4 the moment
                     scoreView.text = obj.second.toString()
 
                     // Add the view to the ranking container
@@ -41,9 +65,7 @@ class Ranking : AppCompatActivity() {
                 }
             }
 
-            override fun onCancelled(error: DatabaseError) {
-                // Handle error
-            }
+            override fun onCancelled(error: DatabaseError) {}
         })
 
         val back: ImageButton = findViewById(R.id.back)
